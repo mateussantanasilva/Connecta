7b07cc745bc43aab3609ffb438c48c85105847bd
mateussantanasilva <santanasilva1778@gmail.com>
2024-09-08 10:37:26 -0300

    🎉 Setup web project

---
+++ web/tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          600: '#316962',
+        },
+        orange: {
+          600: '#DD720F',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
