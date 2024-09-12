4f46820e068e48a99e4345f7435e667d1a8b69e4
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-09-12 08:26:27 -0300

    :sparkles: Função get campaings

--- server/src/routes/get-campaigns.ts
+++ server/src/routes/get-campaigns.ts
@@ -1,8 +1,60 @@
+/* eslint-disable prettier/prettier */
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { ClientError } from '../errors/client-error'
 
-/* export async function getCampaigns(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/campanhas')
-} */
+export async function getCampaigns(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/campanhas',
+    {
+      schema: {
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string().uuid(),
+              name: z.string(),
+              collection_point: z.string(),
+              //              collection_point: z.array(z.string()),
+              description: z.string(),
+              observation: z.string(),
+              categories: z.string(),
+              // categories: z.array(z.string()),
+              progress: z.number(),
+              status: z.string(),
+              // status: z.enum(['Aberta', 'Em_breve', 'Fechada']),
+              participants: z.number(),
+              started_at: z.date(),
+              id_grantee: z.string(),
+              grantee: z.object({
+                full_name: z.string(),
+              }),
+            }),
+          ),
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        const campaigns = await prisma.campaign.findMany({
+          include: {
+            grantee: {
+              select: {
+                full_name: true,
+              },
+            },
+          },
+        })
+
+        if (campaigns.length === 0) {
+          throw new ClientError('Sem campanhas no momento!')
+        }
+
+        return reply.status(200).send(campaigns)
+      } catch (error) {
+        return reply.status(500).send()
+      }
+    },
+  )
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { getCampaigns } from './routes/get-campaigns'
+// import { getCampaignsById } from './routes/get-id-campaigns'
 
 const app = fastify()
 
 app.register(cors, {
   origin: '#',
 })
+
+app.get('/', async (request, reply) => {
+  return 'Hello World!'
+})
+
+app.register(getCampaigns)
+// app.register(getCampaignsById)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
