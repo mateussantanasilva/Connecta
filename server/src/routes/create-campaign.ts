410534d27db762a62ecd4a338f5be055e85c8236
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-09-26 12:02:38 -0300

    Refactor create-campaign.ts to integrate Firebase and improve code formatting

--- server/src/routes/create-campaign.ts
+++ server/src/routes/create-campaign.ts
@@ -1,69 +1,80 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from 'zod';
-import { prisma } from "../lib/prisma";
-import fromZodSchema from "zod-to-json-schema";
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { db } from '../lib/firebase'
+import fromZodSchema from 'zod-to-json-schema'
+import { ClientError } from '../errors/client-error'
+
+const CampaignStatus = z.enum(['ABERTA', 'EM_BREVE', 'FECHADA'])
 
 const campaignSchema = z.object({
-    name: z.string().min(1),
-    collection_point: z.string().min(1),
-    description: z.string().min(1),
-    observation: z.string().optional(),
-    categories: z.string().min(1),
-    progress: z.number().min(0).max(100),
-    status: z.string().min(1),
-    participants: z.number().nonnegative(),
-    started_at: z.coerce.date(),
-    grantee_name: z.string(),
-    grantee_email: z.string().email(),
-    grantee_user_type: z.string().optional().default('grantee'),
-});
+  name: z.string().min(1),
+  collection_point: z.array(z.string()).min(1),
+  description: z.string().min(1),
+  observation: z.string().optional(),
+  categories: z.array(z.string()).min(1),
+  progress: z.number().min(0).max(100),
+  status: CampaignStatus,
+  participants: z.number().nonnegative(),
+  started_at: z.string().min(1),
+  grantee_name: z.string(),
+  grantee_email: z.string().email(),
+  grantee_user_type: z.string().optional().default('grantee'),
+})
 
 export async function createCampaign(app: FastifyInstance) {
-    app.post('/campaign', {
-        schema: {
-            body: fromZodSchema(campaignSchema), // Converte Zod para JSON Schema
-        },
-    }, async (request, reply) => {
-        const {
-            name, collection_point, description, observation, categories, 
-            progress, status, participants, started_at, grantee_name, grantee_email, grantee_user_type
-        } = request.body as z.infer<typeof campaignSchema>;
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/campaigns',
+    {
+      schema: {
+        body: fromZodSchema(campaignSchema),
+      },
+    },
+    async (request, reply) => {
+      const {
+        name,
+        collection_point,
+        description,
+        observation,
+        categories,
+        progress,
+        status,
+        participants,
+        started_at,
+        grantee_name,
+        grantee_email,
+        grantee_user_type,
+      } = request.body as z.infer<typeof campaignSchema>
 
-        try {
-            const campaign = await prisma.campaign.create({
-                data: {
-                    name,
-                    collection_point,
-                    description,
-                    observation,
-                    categories,
-                    progress,
-                    status,
-                    participants,
-                    started_at,
-                    users: {
-                        createMany: {
-                            data: [
-                                {
-                                    full_name: grantee_name,
-                                    email: grantee_email,
-                                    user_type: grantee_user_type
-                                }
-                            ]
-                        }
-                    }
-                }
-            });
-            
-            if (!campaign) {
-                throw new Error('Erro ao criar campanha');
-            }
-
-            return { campaignId: campaign.id };
-        } catch (error) {
-            console.error(error);
-            return reply.status(500).send({ error: 'Server error' });
+      try {
+        const campaignData = {
+          name,
+          collection_point,
+          description,
+          observation,
+          categories,
+          progress,
+          status,
+          participants,
+          started_at: new Date(started_at),
+          grantee: {
+            full_name: grantee_name,
+            email: grantee_email,
+            user_type: grantee_user_type,
+          },
         }
-    });
+
+        const campaignRef = await db.collection('campaigns').add(campaignData)
+
+        if (!campaignRef) {
+          throw new ClientError('Erro ao criar campanha!')
+        }
+
+        return reply.status(201).send({ campaignId: campaignRef.id })
+      } catch (error) {
+        console.error(error)
+        return reply.status(500).send({ error: 'Server error' })
+      }
+    },
+  )
 }
