4624a88edae87154671a97600b192c8789a91a39
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-09-26 12:01:31 -0300

    Refactor firebase.ts to remove unnecessary semicolons

--- server/src/lib/firebase.ts
+++ server/src/lib/firebase.ts
@@ -1,12 +1,9 @@
-import admin from 'firebase-admin';
+import admin from 'firebase-admin'
 
 const serviceAccount = require('../../serviceAccountKey.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
+})
 
-
-
-export const db = admin.firestore(); 
-
+export const db = admin.firestore()
