09f596b5b24afaf989bc49db81fb451026b60074
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-09-26 12:02:55 -0300

    Refactor update-campaign.ts to integrate Firebase and improve code formatting

--- server/src/routes/update-campaign.ts
+++ server/src/routes/update-campaign.ts
@@ -1,53 +1,92 @@
-import type { FastifyInstance } from 'fastify'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+/* eslint-disable camelcase */
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
-import { prisma } from '../lib/prisma'
+import { db } from '../lib/firebase'
+import fromZodSchema from 'zod-to-json-schema'
 import { ClientError } from '../errors/client-error'
-import fromZodSchema from "zod-to-json-schema";
+
+const CampaignStatus = z.enum(['ABERTA', 'EM_BREVE', 'FECHADA'])
 
 const campaignSchema = z.object({
-    name: z.string().min(1),
-    collection_point: z.string().min(1),
-    description: z.string().min(1),
-    observation: z.string().optional(),
-    categories: z.string().min(1),
-    progress: z.number().min(0).max(100),
-    status: z.string().min(1),
-    participants: z.number().nonnegative(),
-    started_at: z.coerce.date(),
-    grantee_name: z.string(),
-    grantee_email: z.string().email(),
-    grantee_user_type: z.string().optional().default('grantee'),
-});
+  name: z.string().min(1),
+  collection_point: z.array(z.string()).min(1),
+  description: z.string().min(1),
+  observation: z.string().optional(),
+  categories: z.array(z.string()).min(1),
+  progress: z.number().min(0).max(100),
+  status: CampaignStatus,
+  participants: z.number().nonnegative(),
+  started_at: z.string().min(1),
+  grantee_name: z.string(),
+  grantee_email: z.string().email(),
+  grantee_user_type: z.string().optional().default('grantee'),
+})
 
 const ParamsSchema = z.object({
-  campaignId: z.string().uuid(),
-});
+  campaignId: z.string(),
+})
 
 export async function updateCampaign(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().put(
     '/campaigns/:campaignId',
     {
-     schema: {
-            body: fromZodSchema(campaignSchema), 
-        },
-    }, async (request, reply) => {
-        const { campaignId } = request.params as z.infer<typeof ParamsSchema>;
+      schema: {
+        params: fromZodSchema(ParamsSchema),
+        body: fromZodSchema(campaignSchema),
+      },
+    },
+    async (request, reply) => {
+      const { campaignId } = request.params as z.infer<typeof ParamsSchema>
+      const {
+        name,
+        collection_point,
+        description,
+        observation,
+        categories,
+        progress,
+        status,
+        participants,
+        started_at,
+        grantee_name,
+        grantee_email,
+        grantee_user_type,
+      } = request.body as z.infer<typeof campaignSchema>
 
-        try {
-            const campaign = await prisma.campaign.update({
-                where: { id: campaignId },
-                data: request.body as z.infer<typeof campaignSchema>,
-            });
-            
-            if (campaignId !== campaign.id) {
-                throw new Error('Erro ao atualizar campanha');
-            }
+      try {
+        const campaignRef = db.collection('campaigns').doc(campaignId)
+        const campaignDoc = await campaignRef.get()
 
-            return { campaignId: campaign.id };
-        } catch (error) {
-            console.error(error);
-            return reply.status(500).send({ error: 'Server error' });
+        if (!campaignDoc.exists) {
+          throw new ClientError('Campanha não encontrada')
         }
-    });
+
+        const updatedCampaignData = {
+          name,
+          collection_point,
+          description,
+          observation,
+          categories,
+          progress,
+          status,
+          participants,
+          started_at,
+          grantee: {
+            full_name: grantee_name,
+            email: grantee_email,
+            user_type: grantee_user_type,
+          },
+        }
+
+        await campaignRef.update(updatedCampaignData)
+
+        return reply.send({ campaignId })
+      } catch (error) {
+        console.error(error)
+        return reply
+          .status(500)
+          .send(new ClientError('Erro ao atualizar campanha'))
+      }
+    },
+  )
 }
