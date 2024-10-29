f407aadf153e81aae7934d0fb4381225a9e4bbe7
Pessinaaa <pedro.pessina99@gmail.com>
2024-10-28 22:55:02 -0300

    :fire: Removing unnecessary routes :truck: Organizing admin routes

--- server/src/routes/admin/admin-routes.ts
+++ server/src/routes/admin/admin-routes.ts
@@ -1,10 +1,16 @@
 import { FastifyInstance } from "fastify"
 import { ZodTypeProvider } from "fastify-type-provider-zod"
 import { getMetrics } from "./get-metrics"
-import { getPublicMetrics } from "./get-public-metrics"
+import { deleteDoneeRequest } from "./delete-donee-request"
+import { getAcceptDoneeRequest } from "./get-accept-donee-request"
+import { getDoneeRequest } from "./get-donee-request"
+import { getDoneeRequests } from "./get-donee-requests"
 
 export async function adminRoutes(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>()
+        .register(deleteDoneeRequest)
+        .register(getAcceptDoneeRequest)
+        .register(getDoneeRequest)
+        .register(getDoneeRequests)
         .register(getMetrics)
-        .register(getPublicMetrics)
 }
--- server/src/routes/user/user-routes.ts
+++ server/src/routes/user/user-routes.ts
@@ -1,26 +1,12 @@
 import { FastifyInstance } from "fastify"
 import { ZodTypeProvider } from "fastify-type-provider-zod"
-import { createUser } from "./create-user"
-import { getUsers } from "./get-users"
 import { getUserById } from "./get-user"
-import { updateUser } from "./update-user"
 import { deleteUser } from "./delete-user"
 import { createDoneeRequest } from "./create-donee-request"
-import { getDoneeRequests } from "./get-donee-requests"
-import { deleteDoneeRequest } from "./delete-donee-request"
-import { getDoneeRequest } from "./get-donee-request"
-import { getAcceptDoneeRequest } from "./get-accept-donee-request"
 
 export async function userRoutes(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>()
-        .register(createUser)
-        .register(getUsers)
         .register(getUserById)
-        .register(updateUser)
         .register(deleteUser)
         .register(createDoneeRequest)
-        .register(getDoneeRequests)
-        .register(getDoneeRequest)
-        .register(deleteDoneeRequest)
-        .register(getAcceptDoneeRequest)
 }
