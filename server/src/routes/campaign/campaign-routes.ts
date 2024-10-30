cb8118eea2f81d4ee437de336c4182a41c08ff5d
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-10-29 22:17:54 -0300

    Refactor campaign routes to organize imports and formatting

--- server/src/routes/campaign/campaign-routes.ts
+++ server/src/routes/campaign/campaign-routes.ts
@@ -1,16 +1,17 @@
-import { FastifyInstance } from "fastify"
-import { ZodTypeProvider } from "fastify-type-provider-zod"
-import { getCampaigns } from "./get-campaigns"
-import { getByIdCampaigns } from "./get-campaign"
-import { createCampaign } from "./create-campaign"
-import { deleteCampaign } from "./delete-campaign"
-import { updateCampaign } from "./update-campaign"
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { getCampaigns } from './get-campaigns'
+import { getByIdCampaigns } from './get-campaign'
+import { createCampaign } from './create-campaign'
+import { deleteCampaign } from './delete-campaign'
+import { updateCampaign } from './update-campaign'
 
 export async function campaignRoutes(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>()
-        .register(getCampaigns)
-        .register(getByIdCampaigns)
-        .register(createCampaign)
-        .register(deleteCampaign)
-        .register(updateCampaign)
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(getCampaigns)
+    .register(getByIdCampaigns)
+    .register(createCampaign)
+    .register(deleteCampaign)
+    .register(updateCampaign)
 }
