2bd6bfeaef7ee70230cf0a4cabb2cbad6af1cec8
Pessinaaa <pedro.pessina99@gmail.com>
2024-11-04 22:34:54 -0300

    :recycle: Refactoring Firebase credential to env

--- server/src/lib/firebase.ts
+++ server/src/lib/firebase.ts
@@ -1,9 +1,18 @@
 import admin from 'firebase-admin'
+import dotenv from 'dotenv'
+import { ClientError } from '../errors/client-error'
 
-const serviceAccount = require('../../serviceAccountKey.json')
+dotenv.config()
+const FIREBASE_KEY = process.env.FIREBASE_KEY
+
+if(!FIREBASE_KEY) {
+  throw new ClientError('Credencial do Firebase não encontrada')
+}
+
+const firebaseKey = JSON.parse(FIREBASE_KEY)
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseKey),
 })
 
 export const db = admin.firestore()
