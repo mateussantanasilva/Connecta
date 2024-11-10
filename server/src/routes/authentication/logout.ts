ee5b1f394b59dfa12c3409ce209874e77cbd2165
Pessinaaa <pedro.pessina99@gmail.com>
2024-11-10 20:17:40 -0300

    :recycle: Fixxing cookie

--- server/src/routes/authentication/logout.ts
+++ server/src/routes/authentication/logout.ts
@@ -4,10 +4,10 @@ import { ZodTypeProvider } from "fastify-type-provider-zod"
 export async function logout(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().get(
         '/logout', async (req, res) => {
-            res.clearCookie('token', { path: '/' })
-            res.clearCookie('user', { path: '/' })
-            res.clearCookie('userData', { path: '/' })
-            return res.redirect('/').status(200)
+            res.clearCookie('token', { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
+            res.clearCookie('user', { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
+            res.clearCookie('userData', { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
+            return res.redirect('https://connecta-test.vercel.app/')
         }
     )
 }
