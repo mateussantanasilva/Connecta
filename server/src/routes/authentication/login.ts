be7983afa0a49fd46fb0585cc84a4c57a1c8a5e0
Pessinaaa <pedro.pessina99@gmail.com>
2024-11-11 11:58:41 -0300

    :recycle: Adding userData to redirect to frontend

--- server/src/routes/authentication/login.ts
+++ server/src/routes/authentication/login.ts
@@ -47,12 +47,14 @@ export async function login(app: FastifyInstance) {
                     }
                 }
                 const jwtUser = await jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' })
+                const userData = (await db.collection('users').doc(userId).get()).data()
+                const jwtUserData = await jwt.sign({ userData }, JWT_SECRET, { expiresIn: '1h' })
                 res.setCookie('user', jwtUser, { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
                 res.setCookie('token', accessToken, { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
                 if(userRole == 'administrador') {
-                    return res.redirect(`https://connecta-test.vercel.app/administrador?token=${encodeURIComponent(accessToken)}&user=${encodeURIComponent(jwtUser)}`)
+                    return res.redirect(`https://connecta-test.vercel.app/administrador?token=${encodeURIComponent(accessToken)}&userId=${encodeURIComponent(jwtUser)}&userData=${encodeURIComponent(jwtUserData)}`)
                 }
-                return res.redirect(`https://connecta-test.vercel.app/perfil?token=${encodeURIComponent(accessToken)}&user=${encodeURIComponent(jwtUser)}`)
+                return res.redirect(`https://connecta-test.vercel.app/perfil?token=${encodeURIComponent(accessToken)}&user=${encodeURIComponent(jwtUser)}&userData=${encodeURIComponent(jwtUserData)}`)
             } catch (error) {
                 console.error('Erro ao fazer login:', error)
                 res.status(500).send(new ClientError('Não foi possível fazer login'))
