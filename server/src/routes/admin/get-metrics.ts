a37ae0328499714ae2a8c29d213ed73f699cf729
Pessinaaa <pedro.pessina99@gmail.com>
2024-11-25 11:03:48 -0300

    Adding new metrics to admin metrics

--- server/src/routes/admin/get-metrics.ts
+++ server/src/routes/admin/get-metrics.ts
@@ -11,6 +11,7 @@ export async function getMetrics(app: FastifyInstance) {
                 const donationsSnapshot = db.collection('donations')
                 const campaignsSnapshot = db.collection('campaigns')
                 const usersSnapshot = db.collection('users')
+                const requestsSnapshot = db.collection('donee-request')
 
                 const donationsData = (await donationsSnapshot.where('status', '==', 'confirmada').get()).docs
                 var annualDonations = 0
@@ -26,11 +27,20 @@ export async function getMetrics(app: FastifyInstance) {
                 const activeDonors = (await usersSnapshot.where('role', '==', 'donatário').get()).size
                 const totalCompletedCampaigns = (await campaignsSnapshot.where('progress', '==', 100).get()).size
                 const openCampaigns = (await campaignsSnapshot.where('status', '==', 'aberta').get()).size
+                const soonCampaigns = (await campaignsSnapshot.where('status', '==', 'em breve').get()).size
+                const doneeRequests = (await requestsSnapshot.get()).size
+                const unconfirmedDonations = (await donationsSnapshot.where('status', '==', 'pendente').get()).size
+                const totalEndedCampaigns = (await campaignsSnapshot.where('status', '==', 'fechada').get()).size
+
 
                 const MetricsSchema = {
                     activeDonors,
+                    doneeRequests,
                     totalCompletedCampaigns,
                     openCampaigns,
+                    totalEndedCampaigns,
+                    soonCampaigns,
+                    unconfirmedDonations,
                     annualDonations
                 }
                 res.status(200).send(MetricsSchema)
