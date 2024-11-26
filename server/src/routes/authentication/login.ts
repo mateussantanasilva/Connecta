c1bd6f5874810607932ac8c367f9ec461f3bef64
Pessinaaa <pedro.pessina99@gmail.com>
2024-11-26 11:54:56 -0300

    Adding expiration to cookie

--- server/src/routes/authentication/login.ts
+++ server/src/routes/authentication/login.ts
@@ -49,7 +49,7 @@ export async function login(app: FastifyInstance) {
                 }
                 const userData = (await db.collection('users').doc(userId).get()).data()
                 const jwtUser = await jwt.sign({ userId, ...userData }, JWT_SECRET, { expiresIn: '1h' })
-                res.setCookie('user', jwtUser, { path: '/', httpOnly: true, secure: true, sameSite: 'none' })
+                res.setCookie('user', jwtUser, { path: '/', httpOnly: true, secure: true, sameSite: 'none', expires: new Date(Date.now() + 3600000) })
                 if(userRole == 'administrador') {
                     return res.redirect(`${redirectURL}/administrador`)
                 }
